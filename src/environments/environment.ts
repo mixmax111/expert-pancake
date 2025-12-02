b8cb7a24fb8d697197ec5d1ce859045f395c6929
mixmax111 <74608679+mixmax111@users.noreply.github.com>
2025-12-02 11:08:00 +0100

    environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   tmdbBaseUrl: 'https://api.themoviedb.org/3',
   tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/w500', // URL per le locandine
-  apiKey: '',
-  apiToken: ''
+  apiKey: '75bfb6c2a0e804c5818d497422cdbf6a',
+  apiToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NWJmYjZjMmEwZTgwNGM1ODE4ZDQ5NzQyMmNkYmY2YSIsIm5iZiI6MTc2NDI0MzM5OS4xNTgwMDAyLCJzdWIiOiI2OTI4MzdjNzQ3NzlkOTkyZGRkOWNmYjgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ofXV10SL1l8FkbehWSsl6kY8Cum-012YNgLANw2kc_0'
 };
